33700b5fa47e82e941ba143983b2f270f2d92e23
Runal <65166862+runalb@users.noreply.github.com>
2022-01-16 22:51:08 +0530

    Added Missing Code

--- ESP32 Code/Final_Code_ESP32/Final_Code_ESP32.ino
+++ ESP32 Code/Final_Code_ESP32/Final_Code_ESP32.ino
@@ -15,11 +15,11 @@ DallasTemperature sensors(&oneWire);
 
 // ------------- WIFI ---------------
 // Replace with your network credentials
-const char* wifi_ssid     = "runal";
-const char* wifi_password = "1234512345";
+const char* wifi_ssid     = "runalb.com - 5G";
+const char* wifi_password = "2702runal@2702";
 
 // REPLACE with your Domain name and URL path or IP address with path
-const char* server_url = "http://192.168.43.75/Hydroponic-WebApp/add-data.php";
+const char* server_url = "http://192.168.1.20/Hydroponic-WebApp/add-data.php";
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,7 +29,19 @@ void setup() {
   // Start the DS18B20 sensor
   sensors.begin();
 
+  // -------- WIFI --------
+  WiFi.begin(wifi_ssid, wifi_password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println("");
+  Serial.println(WiFi.localIP());
   
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -48,17 +60,42 @@ void loop() {
   
   // -------- WIFI --------
   //Check WiFi connection status
-  
+  if(WiFi.status()== WL_CONNECTED){
+    WiFiClient client;
+    HTTPClient http;
 
     // GET query
     String queryString = "?v1=" + String(temperatureC) + "&v2=" + String(temperatureF);
     Serial.print("queryString: ");
     Serial.println(queryString);
     
-    
-    
-      
-    
+    //HTTP
+    http.begin(server_url + queryString); 
+    int httpResponseCode = http.GET();
+
+    // httpCode will be negative on error
+    if(httpResponseCode > 0) {
+      // file found at server
+      if(httpResponseCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+      } 
+      else {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d\n", httpResponseCode);
+      }
+    } 
+    else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpResponseCode).c_str());
+    }
+   
+   
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
   //Send an HTTP GET request every -- seconds
   delay(100000);  
   
