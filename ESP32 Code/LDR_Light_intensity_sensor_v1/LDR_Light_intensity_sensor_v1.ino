6c9d4a07dc72f5de1caf43ad6d5a29b5339e4b1e
runalb <65166862+runalb@users.noreply.github.com>
2022-01-20 22:39:25 +0530

    Final - LDR code

---
+++ ESP32 Code/LDR_Light_intensity_sensor_v1/LDR_Light_intensity_sensor_v1.ino
@@ -0,0 +1,17 @@
+#define LDRPIN 13   // D13 (ADC0) is connected to LDR 
+
+void setup() {
+  // Start the Serial Monitor
+  Serial.begin(115200);
+}
+
+void loop() {
+  // reads the input on analog pin
+  int ldrLightIntensity = analogRead(LDRPIN);
+
+  // Print LDR Sensor Values on Serial Monitor
+  Serial.println("LDR - Light Intensity Sensor: ");
+  Serial.println("  ldrLightIntensity: " + String(ldrLightIntensity));
+  Serial.println(" ");
+  delay(5000);
+}
