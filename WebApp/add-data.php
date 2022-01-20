e6978ed722f1ceb7e01a25ede1a45d93c002f61c
runalb <65166862+runalb@users.noreply.github.com>
2022-01-20 19:44:40 +0530

    added DHT sensor values

--- WebApp/add-data.php
+++ WebApp/add-data.php
@@ -18,12 +18,14 @@
     // Get Data from HTTP request
     $v1 = $_GET["v1"];
     $v2 = $_GET["v2"];
-    $v3 = $_GET["v3"];
-    $v4 = $_GET["v4"];
-    $v5 = $_GET["v5"];
+    $v3 = $_GET["dhtHumidity"];
+    $v4 = $_GET["dhtCelsiusTemperature"];
+    $v5 = $_GET["dhtFahrenheiTemperature"];
+    $v6 = $_GET["heatIndexCelsius"];
+    $v7 = $_GET["heatIndexFahrenheit"];
 
     // SQL Query to add data to the DB Table
-    $sql = "INSERT INTO Sensor_Data (value1, value2, value3, value4, value5) VALUES ('$v1', '$v2', '$v3', '$v4', '$v5')";
+    $sql = "INSERT INTO Sensor_Data (value1, value2, dhtHumidity, dhtCelsiusTemperature, dhtFahrenheiTemperature, heatIndexCelsius, heatIndexFahrenheit) VALUES ('$v1', '$v2', '$v3', '$v4', '$v5', '$v6', '$v7')";
 
     // Add data to DB
     if ($conn->query($sql) === TRUE) {
